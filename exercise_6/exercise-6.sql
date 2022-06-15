aaa03cbb10bc94a4fdce41026dde5598dbc737e9
Alex Orlet <AlexOrlet89@gmail.com>
2022-06-15 16:50:54 -0700

    test 6 down

--- exercise_6/exercise-6.sql
+++ exercise_6/exercise-6.sql
@@ -1,2 +1,8 @@
 -- find all film titles rented out by customers with the first_name Roberta
 -- Similar to exercise 5
+select title
+from film
+INNER JOIN inventory on film.film_id = inventory.film_id
+INNER JOIN rental on rental.inventory_id = inventory.inventory_id
+INNER JOIN customer on customer.customer_id = rental.customer_id
+Where customer.first_name = 'Roberta'
