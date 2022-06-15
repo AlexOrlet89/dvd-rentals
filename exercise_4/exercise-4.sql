20c08064d5f9dbb9fa7c0c84cd19bf05c610ed1a
Alex Orlet <AlexOrlet89@gmail.com>
2022-06-15 16:21:43 -0700

    4th test passes

--- exercise_4/exercise-4.sql
+++ exercise_4/exercise-4.sql
@@ -1,2 +1,6 @@
--- find all customers first_name, last_name, address, and city
--- Similar to exercise 3 but with customer, address, city
+-- find all customers first_name, last_name, addfress, and city
+-- Similar to exercise 3 but with customer, addrfess, city
+select customer.first_name, customer.last_name, address.address, city.city 
+from customer
+LEFT JOIN address on customer.address_id = address.address_id
+LEFT JOIN city on city.city_id = address.city_id
