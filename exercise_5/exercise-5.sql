c5a819b58c385ce09f6b8dc0bb1b00c58b3b99c3
Alex Orlet <AlexOrlet89@gmail.com>
2022-06-15 16:30:34 -0700

    test five passes

--- exercise_5/exercise-5.sql
+++ exercise_5/exercise-5.sql
@@ -1,2 +1,7 @@
 -- find all film titles that an actor with the last_name Davis appears in
 -- Similar to exercise 3 but with actor, film_actor, film
+select title
+from film
+LEFT JOIN film_actor on film.film_id = film_actor.film_id
+LEFT JOIN actor on film_actor.film_id = actor.actor_id
+Where actor.last_name = 'Davis'
